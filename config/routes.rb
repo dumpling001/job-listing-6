ea1d8284e8eef02ebc993172279bb9d7b6078302
dumpling001 <dumpling001@gmail.com>
2017-04-24 16:15:37 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jobs
+
   root 'jobs#index'
+
+  resources :jobs do
+    resources :resumes
+  end
 
   namespace :admin do
     resources :jobs do
